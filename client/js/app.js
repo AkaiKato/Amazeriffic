15ee218cca34958a93e9bcccd73162dcfe429ab9
AkaiKato <rail201@yandex.ru>
2022-03-13 22:02:12 +0300

    end with tags

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,10 @@
 var main = function(toDoObjects) {
     "use strict";
+
     var toDos = toDoObjects.map(function(toDo) {
         return toDo.description;
-    })
+    });
+
     $(".tabs a span").toArray().forEach(function(element) {
         $(element).on("click", function() {
 
@@ -31,6 +33,23 @@ var main = function(toDoObjects) {
 
             } else if ($element.parent().is(":nth-child(3)")) {
 
+                var organizedByTag = tagOrg(toDoObjects);
+
+                organizedByTag.forEach(function(tag) {
+
+                    var $content = $("<ul>");
+                    var $tagName = $("<p>").text(tag.name);
+
+                    tag.todos.forEach(function(description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
+            } else if ($element.parent().is(":nth-child(4)")) {
+
                 var $input = $("<input>");
                 var $button = $("<button>").text("+")
                 $button.on("click", function() {
@@ -39,11 +58,28 @@ var main = function(toDoObjects) {
                 });
                 $content = $("<div>").append($input).append($button);
                 $("main .content").append($content);
+
             }
             return false;
         });
     });
     $(".tabs a:first-child span").trigger("click");
+};
+
+var tagOrg = function(toDoObjects) {
+    var tagList = [],
+        tagsObject = [];
+    toDoObjects.forEach(function(todo) {
+        todo.tags.forEach(function(tag) {
+            if (tagList.includes(tag)) {
+                tagsObject[tagList.indexOf(tag)].todos.push(todo.description);
+            } else {
+                tagList.push(tag);
+                tagsObject.push({ "name": tag, "todos": [todo.description] });
+            }
+        });
+    });
+    return tagsObject;
 };
 
 $(document).ready(function() {
