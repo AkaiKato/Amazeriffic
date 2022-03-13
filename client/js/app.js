d9fc9f47be0f18d230ca030e5abb16afc3337103
AkaiKato <rail201@yandex.ru>
2022-03-13 16:30:45 +0300

    init commit

---
+++ client/js/app.js
@@ -0,0 +1,48 @@
+var main = function() {
+    "use strict";
+    var toDos = [
+        "Закончить писать эту книгу",
+        "Вывести Грейси на прогулку в парк",
+        "Ответить на электронные письма",
+        "Подготовиться к лекции в понедельник",
+        "Обновить несколько новых задач",
+        "Купить продукты"
+    ];
+    $(".tabs a span").toArray().forEach(function(element) {
+        $(element).on("click", function() {
+
+            var $element = $(element),
+                $content;
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+
+                $content = $("<ul>");
+                for (var i = toDos.length; i > -1; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+                $("main .content").append($content)
+
+            } else if ($element.parent().is(":nth-child(2)")) {
+
+                $content = $("<ul>");
+                toDos.forEach(function(todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content)
+
+            } else if ($element.parent().is(":nth-child(3)")) {
+
+                console.log("Щелчок на третьей вкладке!");
+                toDos.push($input.val())
+            }
+            return false;
+        });
+    });
+    $(".tabs a:first-child span").trigger("click");
+};
+
+$(document).ready(main);
