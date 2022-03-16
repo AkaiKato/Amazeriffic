72b453a45864a0a19027944c8603f36890568876
AkaiKato <rail201@yandex.ru>
2022-03-16 21:26:25 +0300

    added connect to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express"),
     http = require("http"),
+    mongoose = require("mongoose"),
     app = express(),
     toDos = [{
             "description": "Купить продукты",
@@ -26,23 +27,54 @@ var express = require("express"),
             "tags": ["писательство", "работа"]
         }
     ]
-http.createServer(app).listen(3000);
+
 app.use(express.static(__dirname + "/client"));
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/todos", function(req, res) {
-    var newToDo = req.body;
-    console.log(newToDo);
-    toDos.push(newToDo);
-    res.json({ "message": "Вы размещаетесь на сервере!" })
+mongoose.connect('mongodb://localhost/amazeriffic');
+
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [String]
 })
+
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+
+http.createServer(app).listen(3000);
 
 
 app.get("/todos.json", function(req, res) {
-    res.json(toDos);
+    ToDo.find({}, function(err, toDos) {
+        if (err !== null) console.log("Error: " + err)
+        else res.json(toDos);
+    })
 });
 
+
 app.post("/todos", function(req, res) {
+    console.log(req.body);
+    var newToDo = new ToDo({
+        "description": req.body.description,
+        "tags": req.body.tags
+    })
+    newToDo.save(function(err, result) {
+        if (err !== null) {
+            console.log("Error: " + err);
+            res.send("ERROR");
+        } else {
+            ToDo.find({}, function(err, result) {
+                if (err !== null) {
+                    res.send("ERROR");
+                } else res.json(result);
+            })
+        }
+    })
+})
+
+
+
+
+/*app.post("/todos", function(req, res) {
     console.log("Данные были отправлены на сервер");
     res.json({ "message": "Вы размещаетесь на сервере!" })
-})
+})*/
